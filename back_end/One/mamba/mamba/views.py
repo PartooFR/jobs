049dd2384bfb8bbdd5a192ed3f0dd20634ec01b8
David MUGISHA <david.mugisha@supelec.fr>
2015-11-30 17:28:41 +0100

    cards are not working :broken_heart:

--- back_end/One/mamba/mamba/views.py
+++ back_end/One/mamba/mamba/views.py
@@ -2,18 +2,6 @@ from pyramid.view import view_config
 from pyramid.response import Response
 import requests
 
-list1= [{
-      "id": "4eea4ffc91e31d1746000046",
-      "name": "Example Board",
-      "desc": "This board is used in the API examples",
-      "shortUrl": "https://trello.com/b/OXiBYZoj"
-  }, {
-      "id": "4ee7e707e582acdec800051a",
-      "name": "Public Board",
-      "desc": "A board that everyone can see",
-      "shortUrl": "https://trello.com/b/IwLRbh3F"
-  }]
-
 urlApi = 'https://api.trello.com'
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
@@ -22,7 +10,10 @@ def my_view(request):
 
 @view_config(route_name='index', renderer='templates/index.pt')
 def index(request):
-    return {'title': 'Mamba to Trello'}
+    if 'form.submitted' in request.params:
+        name= request.params['username']
+        return {'title': 'Mamba to Trello', 'text':request.form['text']}
+    return {'title': 'Mamba to Trello', 'text':'trello'}
     
 @view_config(route_name='authenticate', renderer='templates/authenticate.pt')
 def authenticate(request):
@@ -30,20 +21,23 @@ def authenticate(request):
     
 @view_config(route_name='boards', renderer='templates/boards.pt')
 def boards(request):
-    url =  urlApi + '/1/members/trello/boards'
+    username =  request.matchdict['username']
+    url =  urlApi + '/1/members/'+username+'/boards'
     response_dict = requests.get(url).json()
     return {'boards': response_dict}
-
-@view_config(route_name='cards', renderer='templates/cards.pt')
-def cards(request):
-    board_id = request.matchdict['board_id']
-    response_dict = requests.get(urlApi + '/1/boards/'+board_id+'/cards').json()
-    return {'cards': response_dict}
     
 @view_config(route_name='lists', renderer='templates/lists.pt')
 def cards(request):
     board_id = request.matchdict['board_id']
-    response_dict = requests.get(urlApi + '/1/boards/'+board_id+'/lists').json()
+    url = urlApi + '/1/boards/'+board_id+'/lists'
+    response_dict = requests.get(url).json()
     return {'lists': response_dict}
+    
+@view_config(route_name='cards', renderer='templates/cards.pt')
+def cards(request):
+    board_id = request.matchdict['board_id']
+    url = urlApi + '/1/boards/'+board_id+'/cards'
+    response_dict = requests.get(url).json()
+    return {'cards': response_dict}
 
     
