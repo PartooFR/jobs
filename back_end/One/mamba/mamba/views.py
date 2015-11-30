d32e8c5efebbb312512e6025510e588971f5c316
David MUGISHA <david.mugisha@supelec.fr>
2015-11-30 22:23:01 +0100

    Cards fixed :unamused:

--- back_end/One/mamba/mamba/views.py
+++ back_end/One/mamba/mamba/views.py
@@ -4,6 +4,7 @@ import requests
 from pyramid.httpexceptions import HTTPFound
 
 urlApi = 'https://api.trello.com'
+appKey = '464961dc9bfdd33036f0131101b6d72d'
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
@@ -16,10 +17,6 @@ def index(request):
         return HTTPFound(location=request.route_url('boards', username=name))
     return {'title': 'Mamba to Trello', 'text':'trello'}
     
-@view_config(route_name='authenticate', renderer='templates/authenticate.pt')
-def authenticate(request):
-    return {'title': 'Authenticate'}
-    
 @view_config(route_name='boards', renderer='templates/boards.pt')
 def boards(request):
     username =  request.matchdict['username']
@@ -28,7 +25,7 @@ def boards(request):
     return {'boards': response_dict}
     
 @view_config(route_name='lists', renderer='templates/lists.pt')
-def cards(request):
+def lists(request):
     board_id = request.matchdict['board_id']
     url = urlApi + '/1/boards/'+board_id+'/lists'
     response_dict = requests.get(url).json()
@@ -40,5 +37,19 @@ def cards(request):
     url = urlApi + '/1/boards/'+board_id+'/cards'
     response_dict = requests.get(url).json()
     return {'cards': response_dict}
+    
+@view_config(route_name='authenticate', renderer='templates/authenticate.pt')
+def authenticate(request):
+    if 'form.submit' in request.params:
+        token= request.params['token']
+        return HTTPFound(location=request.route_url('myboard', token=token))
+    return {'title': 'Authenticate', 'token': ''}
+    
+@view_config(route_name='myboard', renderer='templates/boards.pt')
+def myboard(request):
+    token =  request.matchdict['token']
+    url =  urlApi+'/1/members/me?fields=username,fullName,url&boards=all&board_fields=name&organizations=all&organization_fields=displayName&key='+appKey+'&token='+token
+    response_dict = requests.get(url).json()
+    return {'boards': response_dict}
 
     
